5d1ae111a6162fd2699802840cabbc58f9ae2ab5
sumeka <sumeka.arasakumar@aspiresys.com>
2025-10-23 18:15:25 +0530

    colors.ts in styles folder

---
+++ src/styles/colors.ts
@@ -0,0 +1,28 @@
+export const colors={
+    PRIMARY_COLOR: '#FF0000',
+    SECONDARY_COLOR: '#084a08ff',
+    white:'#fff',
+    black:'#000',
+    formColor:'#585454ff',
+    PRIMARY_BACKGROUNDCOLOR:'#fff',
+    ORANGE_COLOR:'#ea8642ff',
+    grey:"#888",
+    red:'red',
+    orange:"#ea8642",
+
+    //Intro screen
+    inActive:"#ccc",
+
+    //Signup and Login - AUTH icons color
+    google:'#4477c4ff',
+    faceBook: "#465ee6ff",
+    twitter: "#319ec6ff",
+
+    //product detail -container
+    containerBorder:'#ecb385ff',
+    containerBackground:"#e4b7a0ff"
+    
+
+
+    
+}
