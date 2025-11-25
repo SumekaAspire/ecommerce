fe8e376145f46eb061ea5e3ff4d8cf55b804cabd
sumeka <sumeka.arasakumar@aspiresys.com>
2025-11-25 18:31:57 +0530

    Wishlist.tsx - updated the code redux to store the wishlist product HomeBanners.tsx -  upadted colors Homescreen.tsx - updated the search bar component (with modal view)

--- src/components/Wishlist.tsx
+++ src/components/Wishlist.tsx
@@ -2,25 +2,37 @@ import { View, Text, TouchableOpacity, StyleSheet,ViewStyle} from 'react-native'
 import React,{useState} from 'react'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { colors } from '../styles/colors';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleWishlist, wishlistSelector } from '../store/slices/wishlistSlice';
 
 //interface foe whislist
-interface Whishlist{
+interface Wishlist{
    style?: ViewStyle,
+   //productId:string,
+   product:{
+    id: number;
+    title: string;
+    image?: string;
+    price?: number;
+   }
 }
 /**
  * Wishlist component
- * @returns toggle Wishlist
+ * @returns whishlist icon- can be toggle (added or removed to Wishlist)
  */
-const Whislist: React.FC<Whishlist>= ({style}) => {
-    const [isWishlisted, setIsWishlisted] = useState(false);
+const Wishlist: React.FC<Wishlist>= ({style,product}) => {
+   const dispatch = useDispatch();
+   const wishlistItems = useSelector(wishlistSelector);
+   const isWishlisted = wishlistItems.some(item => item.id === product.id)
 
-    const toggleWishlist =() =>{
-        setIsWishlisted(prev => !prev)
+    //handle toggleWishlist
+    const handleToggle =() =>{
+       dispatch(toggleWishlist(product));
     }
   return (
      <TouchableOpacity
         style={style}
-        onPress={toggleWishlist} >
+        onPress={handleToggle} >
       <Ionicons
          name={isWishlisted ? "heart" : "heart-outline"}
          size={24}
@@ -31,5 +43,49 @@ const Whislist: React.FC<Whishlist>= ({style}) => {
   )
 }
 
-export default Whislist
+export default Wishlist
+
+
+
+
+
+
+
+
+
+
+// import { View, Text, TouchableOpacity, StyleSheet,ViewStyle} from 'react-native'
+// import React,{useState} from 'react'
+// import Ionicons from 'react-native-vector-icons/Ionicons';
+// import { colors } from '../styles/colors';
+
+// //interface foe whislist
+// interface Whishlist{
+//    style?: ViewStyle,
+// }
+// /**
+//  * Wishlist component
+//  * @returns toggle Wishlist
+//  */
+// const Whislist: React.FC<Whishlist>= ({style}) => {
+//     const [isWishlisted, setIsWishlisted] = useState(false);
+
+//     const toggleWishlist =() =>{
+//         setIsWishlisted(prev => !prev)
+//     }
+//   return (
+//      <TouchableOpacity
+//         style={style}
+//         onPress={toggleWishlist} >
+//       <Ionicons
+//          name={isWishlisted ? "heart" : "heart-outline"}
+//          size={24}
+//          color={isWishlisted ? colors.red : colors.black}
+//       />
+//      </TouchableOpacity>
+   
+//   )
+// }
+
+// export default Whislist
 
